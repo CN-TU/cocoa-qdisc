ecaa3fb9070dc2d4e17a350a4b9fb70eeaa2d65c
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2019-09-24 16:59:40 +0200

    Setup seems to work

--- test.py
+++ test.py
@@ -28,15 +28,19 @@ import statistics
 # X = 10
 # NUMPING = 1000
 
+# Client port
+cport = 60000
 # milliseconds
-delay_to_add = 100
+delay_to_add = 10
 # Mbit/s
 rate = 100
 # Bytes, obviously
 MTU = 1514
-QDISC_NAME = "fq"
-
+QDISC_NAME = "cn"
+#             Mbit/s       ms                bytes
 BDP_packets = rate*1000000*delay_to_add/1000/(MTU*8)
+# Just for fun...
+BDP_packets *= 0.5
 # BDP_packets = 1
 print("BDP_packets", BDP_packets)
 # quit()
@@ -51,7 +55,7 @@ def run_commands(cmds, Popen=False):
 		else:
 			kwargs = {}
 		try:
-			print("cmd", cmd)
+			print("cmd", cmd, "kwargs", kwargs)
 			if not Popen:
 				output = subprocess.run(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True, **kwargs)
 				# print("output", output)
@@ -66,9 +70,21 @@ def run_commands(cmds, Popen=False):
 
 
 env_with_tc = os.environ.copy()
-env_with_tc["TC_LIB_DIR"] = os.path.expanduser('~/repos/iproute2/tc')
+# Idiotic
+# env_with_tc["TC_LIB_DIR"] = os.path.expanduser('~/repos/iproute2/tc')
+env_with_tc["TC_LIB_DIR"] = "/home/max/repos/iproute2/tc"
 
 # print("os.environ", os.environ)
+
+def execute_popen_and_show_result(command, host=None):
+	parent = host if host is not None else subprocess
+	print(f"Executing{f' on host {host.name}' if host else ''}", command)
+	with parent.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as cmd:
+		out, err = cmd.stdout.read(), cmd.stderr.read()
+		if out:
+			print("out", out.decode("utf-8"))
+		if err:
+			print("err", err.decode("utf-8"))
 
 def run(vnet):
 		"Main functionality"
@@ -84,41 +100,50 @@ def run(vnet):
 		switch = vnet.Switch("sw")
 		hosts = []
 		for i in range(2):
-				host = vnet.Host("host{}".format(i))
-				host.connect(vnet.VirtualLink, switch, "eth0")
-				host["eth0"].add_ip(network)
-				hosts.append(host)
-				# print("host", host)
+			host = vnet.Host("host{}".format(i))
+			host.connect(vnet.VirtualLink, switch, "eth0")
+			host["eth0"].add_ip(network)
+			execute_popen_and_show_result("ethtool -K eth0 gro off", host)
+			execute_popen_and_show_result("ethtool -K eth0 gso off", host)
+			execute_popen_and_show_result("ethtool -K eth0 tso off", host)
+			hosts.append(host)
+			# print("host", host)
 		# hosts[0]["eth0"].tc('add', 'netem', delay=DELAY, jitter=SIGMA, dist=y)
 
 		# import pdb; pdb.set_trace()
 
+		# print("switch.interfaces", switch.interfaces)
 		for interface in switch.interfaces:
-			run_commands(["tc qdisc add dev {} root handle 1: netem delay {}ms".format(interface, delay_to_add/2), "tc qdisc add dev {} parent 1: handle 2: htb default 21".format(interface), "tc class add dev {} parent 2: classid 2:21 htb rate {}mbit".format(interface, rate,), ("tc qdisc add dev {} parent 2:21 handle 3: {} nopacing flow_limit {}".format(interface, QDISC_NAME, int(math.ceil(BDP_packets))), {"env": env_with_tc})])
+			execute_popen_and_show_result(f"ethtool -K {interface} gro off")
+			execute_popen_and_show_result(f"ethtool -K {interface} gso off")
+			execute_popen_and_show_result(f"ethtool -K {interface} tso off")
+
+			run_commands([f"tc qdisc add dev {interface} root handle 1: netem delay {delay_to_add/2}ms", f"tc qdisc add dev {interface} parent 1: handle 2: htb default 21", f"tc class add dev {interface} parent 2: classid 2:21 htb rate {rate}mbit", (f"tc qdisc add dev {interface} parent 2:21 handle 3: {QDISC_NAME} nopacing flow_limit {int(math.ceil(BDP_packets))}", {"env": env_with_tc})])
 			# run_commands(["tc qdisc add dev {} root handle 1: netem delay {}ms".format(interface, delay_to_add/2), "tc qdisc add dev {} parent 1: handle 2: htb default 21".format(interface), "tc class add dev {} parent 2: classid 2:21 htb rate {}mbit ceil {}mbit".format(interface, rate, rate), ("tc qdisc add dev {} parent 2:21 handle 3: {}".format(interface, QDISC_NAME), {"env": env_with_tc})])
 		#     # output = subprocess.run(f"tc qdisc replace dev {interface} root {QDISC_NAME}".split(" "), capture_output=True, env=env_with_tc)
 		#     print("output", output)
 		vnet.update_hosts()
 
-		with hosts[0].Popen("ping -c 100 -i 0 host1".split(), stdout=subprocess.PIPE) as ping:
+		with hosts[0].Popen("ping -c 100 -i 0 host1".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as ping:
 			ping_output = ping.stdout.read().decode("utf-8").split("\n")
 			ping_output = [float(item.split()[-2][5:]) for item in ping_output if "time=" in item]
 			print("mean rtt", statistics.mean(ping_output))
 
-			# print("ping stdout", ping.stdout.read().decode("utf-8"))
-			# print("ping stderr", ping.stderr.read().decode("utf-8"))
-		return
-
 		server_popen = hosts[1].Popen("iperf3 -4 -s".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		# time.sleep(0.1)
-		client_popen = hosts[0].Popen(f"iperf3 -4 -t 60 -Z reno -c host1".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		tcpdump_sender_popen = hosts[0].Popen(f"/usr/sbin/tcpdump -s 96 -i eth0 -w sender.pcap tcp port {cport} && port 5201".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		tcpdump_receiver_popen = hosts[1].Popen(f"/usr/sbin/tcpdump -s 96 -i eth0 -w receiver.pcap tcp port {cport} && port 5201".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		tcpdump_switch_popens = []
+		for interface_name in switch.interfaces.keys():
+			tcpdump_switch_popens.append(subprocess.Popen(f"/usr/sbin/tcpdump -s 96 -i {interface_name} -w switch_{interface_name}.pcap tcp port {cport} && port 5201".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+
+		client_popen = hosts[0].Popen(f"iperf3 -4 -C reno --cport {cport} -c host1".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		print("client pid", client_popen.pid)
 		# time.sleep(0.1)
 
-		trace_popen = hosts[0].Popen(f"trace-cmd record -e tcp:tcp_probe -P {client_popen.pid}".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		# trace_popen = hosts[0].Popen(f"trace-cmd record -e tcp:tcp_probe -P {client_popen.pid}".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		# trace_popen = hosts[0].Popen(f"trace-cmd record -e tcp:tcp_probe".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-		# tcpdump_popen = hosts[0].Popen(f"/usr/sbin/tcpdump -s 96 -i eth0 -w out.pcap".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 		out, err = client_popen.communicate()
 		if out:
@@ -133,19 +158,34 @@ def run(vnet):
 		if err:
 			print("server err", err.decode("utf-8"))
 
-		trace_popen.send_signal(signal.SIGINT)
-		out, err = trace_popen.stdout.read(), trace_popen.stderr.read()
-		if out:
-			print("trace out", out.decode("utf-8"))
-		if err:
-			print("trace err", err.decode("utf-8"))
-
-		# tcpdump_popen.terminate()
-		# out, err = tcpdump_popen.stdout.read(), tcpdump_popen.stderr.read()
+		# trace_popen.send_signal(signal.SIGINT)
+		# out, err = trace_popen.stdout.read(), trace_popen.stderr.read()
 		# if out:
-		# 	print("tcpdump out", out.decode("utf-8"))
+		# 	print("trace out", out.decode("utf-8"))
 		# if err:
-		# 	print("tcpdump err", err.decode("utf-8"))
+		# 	print("trace err", err.decode("utf-8"))
+
+		tcpdump_sender_popen.terminate()
+		out, err = tcpdump_sender_popen.stdout.read(), tcpdump_sender_popen.stderr.read()
+		if out:
+			print("tcpdump out", out.decode("utf-8"))
+		if err:
+			print("tcpdump err", err.decode("utf-8"))
+
+		tcpdump_receiver_popen.terminate()
+		out, err = tcpdump_receiver_popen.stdout.read(), tcpdump_receiver_popen.stderr.read()
+		if out:
+			print("tcpdump out", out.decode("utf-8"))
+		if err:
+			print("tcpdump err", err.decode("utf-8"))
+
+		for index, interface_name in enumerate(switch.interfaces.keys()):
+			tcpdump_switch_popens[index].terminate()
+			out, err = tcpdump_switch_popens[index].stdout.read(), tcpdump_switch_popens[index].stderr.read()
+			if out:
+				print(f"{interface_name} out", out.decode("utf-8"))
+			if err:
+				print(f"{interface_name} err", err.decode("utf-8"))
 
 		# if out:
 		# 	print("server out", out)
